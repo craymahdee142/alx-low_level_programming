497be47bce6c0dcdf37f1b3f7fcc43e233e00ef1
skizzo142 <craymahdaniel@gmail.com>
2023-02-17 20:50:53 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
- * main - A program that prints base ten digits
- * Return: 0 (Success)
+ * main - A program that print all sigle base number
+ *
+ * Return: 0 (Succes)
  */
 int main(void)
 {
-	int a;
+	int n;
 
-	for (a = 0; a <= 10; a++)
-		printf("%d", a);
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
 	putchar('\n');
 	return (0);
 }
+
+
