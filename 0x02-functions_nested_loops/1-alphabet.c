8d0c7547406794f3726e9cf031846928b421415b
skizzo142 <craymahdaniel@gmail.com>
2023-02-21 16:43:18 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+ /**
+ * main - Write a function that prints alphabet in lowercase.
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char letter_lc;
+
+	for (letter_lc = 'a'; letter_lc <= 'z'; letter_lc++)
+	{
+		_putchar(letter_lc);
+	}
+	_putchar('\n');
+	return (0);
+}
