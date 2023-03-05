e18b7a96a009d6b76954a916454724d8b75e1f37
skizzo142 <craymahdaniel@gmail.com>
2023-03-05 15:42:40 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,12 @@
 **/
 char *_strchr(char *s, char c)
 {
-	char str[];
-	char c = 'i';
-	char *p;
+	int n;
 
-	p = strchr(str, c);
-
+	for (n = 0; s[n]>= '\0'; n++)
+	{
+		if (s[n] == c)
+		return (s + n);
+	}
 	return (NULL);
 }
