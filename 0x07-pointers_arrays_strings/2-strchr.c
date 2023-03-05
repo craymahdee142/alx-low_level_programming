1e33a3b2f558f931e87217ccf6bc9b1785e1972f
skizzo142 <craymahdaniel@gmail.com>
2023-03-05 15:31:55 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#includ "main.h"
+
+/**
+ * *_strchr - function that locates chr in string
+ * @s: input
+ * @c: input
+ * Return: null
+**/
+char *_strchr(char *s, char c)
+{
+	char str[];
+	char c = 'i';
+	char *p;
+
+	p = strchr(str, c);
+
+	return (NULL);
+}
