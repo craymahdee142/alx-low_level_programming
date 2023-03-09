543e3300039a50c29feb99ed0c2c9bcc7e4dbb61
skizzo142 <craymahdaniel@gmail.com>
2023-03-08 19:07:37 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,13 +7,10 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	len = 0;
-
 	if (*s != '\0')
 	{
-		len++;
-		len += _strlen_recursion(s + 1);
+		return (0);
 	}
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
