b9d00dc15fab73ba0394e1777a0615f6ada783a8
skizzo142 <craymahdaniel@gmail.com>
2023-03-14 15:39:39 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,16 +11,14 @@ char *_strdup(char *str)
 	unsigned int size, i;
 	char *ptr;
 
-	i = 0;
-	size = 0;
-
 	if (str == NULL)
 		return (NULL);
 	for (size = 0; str[size] != '\0'; size++)
-	ptr =  malloc(sizeof(char) * (len + 1));
+		;
+	ptr =  malloc(sizeof(char) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; i <= size; i++)
 		ptr[i] = str[i];
 	return (ptr);
 }
