81cd5ac5d7e77de62beadf72ea968f9d89713a28
skizzo142 <craymahdaniel@gmail.com>
2023-03-14 14:43:00 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,17 +8,17 @@
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
+	char *ptr;
 	unsigned int i, len;
 
 	if (str == NULL)
 		return (NULL);
 	while (str[len])
 		len++;
-	duplicate = malloc(sizeof(char) * (len - 1));
-	if (duplicate == NULL)
+	ptr = (char*)malloc((size + 1) * sizeof(char);
+	if (ptr == NULL)
 		return (NULL);
-	while ((duplicate[i] = str[i]) != '\0')
-		i++;
-	return (duplicate);
+	for (i = 0; str[i] != '\0'; i++)
+		ptr[i] = str[i]
+	return (ptr);
 }
