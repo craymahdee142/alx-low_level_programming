fad2aabae35d08fd3a66db1cdb1a4b76b7d160d0
skizzo142 <craymahdaniel@gmail.com>
2023-03-20 15:58:05 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _HEADER_
+#define -HEADER_
 
 /**
  *struct dog - a dog's basix info
@@ -15,9 +15,10 @@ struct dog
 	char *owner;
 };
  /**
- *dog_t - typedef fofr the struct dog
+ *dog_t - typedef for the struct dog
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
