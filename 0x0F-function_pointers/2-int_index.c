f7d619e30cd0b8ba011941bccc7e933a9f5ebb4b
skizzo142 <craymahdaniel@gmail.com>
2023-03-22 16:22:48 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,23 +1,28 @@
-i#include "function_pointers.h"
-
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- *int_index - search for intergers
- *@array: array with data
- *@size: size of array
- *@cmp: func pointer
- *Return: intergers count
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array)[i] != 0)
-			return (0);
-		}
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
 	return (-1);
 }
