03fa0f79da29cf140f3e8b3c58454aa7b60d7c69
skizzo142 <craymahdaniel@gmail.com>
2023-03-23 16:11:56 -0700

    1-array_iteartor.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,15 +6,16 @@
  *array_iterator - func that executes func given parameter of the elemets
  *@array: array
  *@size: how many element to prints
- *@action: pointer to print in regular 
+ *@action: pointer to print in regular
  *Return: void
  */
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int i;
+
 	if (array == NULL || action == NULL)
 		return;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
