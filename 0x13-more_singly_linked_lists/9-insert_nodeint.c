642ae58040df79d84086774e0254a5edb906971a
skizzo142 <craymahdaniel@gamil.com>
2023-04-05 22:58:54 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,34 +1,60 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- *insert_nodeint_at_index - insert a new node
- *@head: Pointer to the address of the head
- *@idx: the index of the listint where the should be added
- *@n: interger of the new node
- *Return: if the function failss - else address
+ *listint_len - function that returns the no of elements in a listint_t list
+ *@h: pointer to a address
+ *Return: number of elements
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigend int idx, int n)
-{
-	listint_t *new_node, *copy = *head;
-	unsigned int i;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+
+	}
+	return (count);
+
+}
+
+/**
+ *insert_nodeint_at_index - function to insert a new node at a given position
+ *@head: pointer to the beginning of a listint_t list
+ *@idx: index where new will be added
+ *@n: nember for the new node
+ *Return: address of the new node or NULL on failure
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	size_t len;
+	unsigned int j;
+	listint_t *previous = *head, *recent;
+
+	len = listint_len(*head);
+
+	if (idx > len)
 		return (NULL);
-	new_node->n = n;
+	new = malloc(sizeof(listint_t));
+	if (recent == NULL)
+		return (NULL);
+	recent->n = n;
+
 	if (idx == 0)
 	{
-		new_node->next = copy;
-		*head = new_node;
-		return (new_node);
+		recent->next = *head;
+		*head = recent;
+		return (recent);
 	}
-	for (i = 0; i < (idx - 1); i++)
-	{
-		if (copy == NUUL || copy->next == NULL)
-			return (NULL);
-	}
-	new_node->next = copy->next;
-	copy->next = new_node;
-	return (NULL);
+	for (j = 0; j < idx - 1; j++)
+		previous = previous->next;
+	recent->next = previous->next;
+	previous->next = recent;
+	return (recent);
+
+}
 }
