8d6a95e428f0e702a460852f2c6f789bd6a18402
skizzo142 <craymahdaniel@gamil.com>
2023-04-07 10:01:45 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -12,15 +12,19 @@
 unsigned int binary_to_unit(const char *b)
 {
 	unsigned int num = 0;
-	int i, power = 0;
+	int i = 0;
+	int power = 0;
 
 	if (b == NULL)
 		return (0);
-	for (i = strlen(b) - 1; i >= 0; i--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		num += (unsigned int)(b[i] - '0') * (unsigned int)pow(2, power);
+	}
+	for (i = strlen(b) - 1; i >= 0; i--)
+	{
+		num += (unsigned int)(b[i] - '0') << power;
 		power++;
 	}
 	return (num);
