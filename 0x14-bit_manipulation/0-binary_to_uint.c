2b79b5e68cae6cab2009eba3150de5c9588aaf23
skizzo142 <craymahdaniel@gamil.com>
2023-04-07 11:21:34 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_unit(const char *b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] < '0' && b[i] > '1')
 			return (0);
 	/*}
 	for (i = strlen(b) - 1; i >= 0; i++)
