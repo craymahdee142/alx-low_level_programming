27a1c10ed167cc1a3925a9db1532ce99a0df1e2e
skizzo142 <craymahdaniel@gamil.com>
2023-04-07 08:35:41 -0700

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -4,7 +4,7 @@
 #include <math.h>
 
 /**
- *binary_t-unit - function that converts binary to an unsigned int
+ *binary_to_unit - function that converts binary to an unsigned int
  *@b: Pointing to a string of 0 and 1 chars
  *Description: length of binary string to unit
  *Return: converted number
@@ -12,15 +12,15 @@
 unsigned int binary_to_unit(const char *b)
 {
 	unsigned int num = 0;
-	int power = 0, int i;
+	int i, power = 0;
 
-	if (b[i] < '0' || b[i] > '1')
+	if (b == NULL)
 		return (0);
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = strlen(b) - 1; i >= 0; i++)
 	{
-		if (b == NULL)
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		num += (unsigned int)pow(2, power);
+		num += (unsigned int)(b[i] - '0') * (unsigned int)pow(2, power);
 		power++;
 	}
 	return (num);
