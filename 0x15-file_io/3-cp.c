0d6316442c3e0d1b046d6197caf1107b770a18e0
skizzo142 <craymahdaniel@gamil.com>
2023-04-27 02:17:32 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,72 @@
+#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
-void check_IO_stat(int stat, int fd, char *filename, char mode);
 /**
-* main - copies the content of one file to another
-* @argc: argument count
-* @argv: arguments passed
-*
-* Return: 1 on success, exit otherwise
-*/
+ *error_file - check if file can be opened.
+ *@file_from: file_from.
+ *@file_to: file
+ *@argv: arguments vector.
+ *Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+/**
+ *main - check the code for Holberton School students.
+ *@argc: number of arguments.
+ *@argv: arguments vector.
+ *Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-int src, dest, n_read, wrote, close_src, close_dest;
-unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-char buffer[1024];
+	int file_from, file_to, err_close;
+	ssize_t nchars, nwr;
+	char buf[1024];
 
-if (argc != 3)
-{
-fprintf(stderr, "Usage: cp file_from file_to\n");
-exit(EXIT_FAILURE);
-}
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
 
-src = open(argv[1], O_RDONLY);
-if (src == -1)
-{
-perror("Error opening file for reading");
-exit(EXIT_FAILURE);
-}
-dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-if (dest == -1)
-{
-perror("Error opening file for writing");
-exit(EXIT_FAILURE);
-}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
-while ((n_read = read(src, buffer, sizeof(buffer))) > 0)
-{
-wrote = write(dest, buffer, n_read);
-if (wrote == -1)
-{
-perror("Error writing to file");
-exit(EXIT_FAILURE);
-}
-}
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(file_from, buf, 1024);
+		if (nchars == -1)
+			error_file(-1, 0, argv);
+		nwr = write(file_to, buf, nchars);
+		if (nwr == -1)
+			error_file(0, -1, argv);
+	}
 
-if (n_read == -1)
-{
-perror("Error reading from file");
-exit(EXIT_FAILURE);
-}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
 
-close_src = close(src);
-if (close_src == -1)
-{
-perror("Error closing file descriptor for input file");
-exit(EXIT_FAILURE);
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
 }
 
-close_dest = close(dest);
-if (close_dest == -1)
-{
-perror("Error closing file descriptor for output file");
-exit(EXIT_FAILURE);
-}
-
-return (0);
-}
