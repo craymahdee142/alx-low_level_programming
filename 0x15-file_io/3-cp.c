a0c648598486ba51f89933b6c57c8f0b3f7beb72
skizzo142 <craymahdaniel@gamil.com>
2023-04-26 17:15:51 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,66 +1,53 @@
-#include "main.h"
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-/**
- *int main - cp file_from file_to
- *@argc: argument count
- *@argv: argument string
- *Return: 0 (success)
- */
-int main(int argc, char *argv[])
-{
-	FILE *fp1, *fp2;
-	int count = 0;
-	char *text;
+#include <unistd.h>
+#include <fcntl.h>
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: cp file from file_to\n");
-		exit(97);
-	}
-	const char *file_from = argv[1];
-	const char *file_to = argv[2];
+#define BUFFER_SIZE 1024
 
-	fp1 = fopen(file_from, "r");
-	if (fp1 == NULL)
-	{
-		fprintf(stderr, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-	text = (char *)malloc(1025 * sizeof(char));
-	if (text == NULL)
-	{
-		fprintf(stderr, "Error: Failed to alloacte memory\n");
-		exit(99);
-	}
-	while ((count = fread(text, sizeof(char), 1024, fp1)) > 0)
-	{
-		text[count] = '\0';
-		if (fputs(text, stdout) < 0)
-		{
-			fprintf(stderr, "Error: Can't write to file %s\n", file_to);
-			exit(99);
-		}
-	}
-	if (ferror(fp1))
-	{
-		fprintf(stderr, "Error: Failed to read from file %s\n", file_from);
-		exit(98);
-	}
-	fclose(fp1);
-	fp2 = fopen(file_to, "w");
-	if (fp2 == NULL)
-	{
-		fprintf(stderr, "Error: Can't write to file %s\n", file_to);
-		exit(99);
-	}
-	if (fclose(fp2) == EOF)
-	{
-		fprintf(stderr, "Error: Can't close file descripter %d\n", fileno(fp2));
-		exit(100);
-	}
-	free(text);
-	return (0);
+int main(int argc, char *argv[]) {
+    int source_fd, dest_fd, bytes_read, bytes_written;
+    char buffer[BUFFER_SIZE];
+
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s source_file destination_file\n", argv[0]);
+        exit(1);
+    }
+
+    source_fd = open(argv[1], O_RDONLY);
+    if (source_fd == -1) {
+        perror("open");
+        exit(2);
+    }
+
+    dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    if (dest_fd == -1) {
+        perror("open");
+        exit(3);
+    }
+
+    while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(dest_fd, buffer, bytes_read);
+        if (bytes_written == -1) {
+            perror("write");
+            exit(4);
+        }
+    }
+
+    if (bytes_read == -1) {
+        perror("read");
+        exit(5);
+    }
+
+    if (close(source_fd) == -1) {
+        perror("close");
+        exit(6);
+    }
+
+    if (close(dest_fd) == -1) {
+        perror("close");
+        exit(7);
+    }
+
+    return 0;
 }
-
