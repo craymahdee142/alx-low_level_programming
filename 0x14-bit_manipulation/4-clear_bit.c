76d9def6482e598390d53d59c5d32944d69eefb1
craymahdee142 <craymahdaniel@gmail.com>
2023-05-04 15:41:13 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *clear_bit - function that clears bit to 0
+ *@n: Pointer to interger
+ *@index: unsigned index
+ *Return: 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	/* check if index is greater than 31 */
+	if (index > 31)
+	{
+		return (-1);
+	}
+	*n &= ~(1UL << index);
+	return (1);
+}
+
