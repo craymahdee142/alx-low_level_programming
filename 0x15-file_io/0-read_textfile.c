ce8ce8b705a8b3f1c9b3859fcb67de9864699f93
craymahdee142 <craymahdaniel@gmail.com>
2023-05-08 15:49:15 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,20 +7,20 @@
  *@letters: number of char to be read
  *Return: 0 (success)
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+size_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	size_t count = 0;
-	char ch;
+	char c;
 
 	if (filename == NULL)
 		return (0);
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
-	while ((count < letters) && ((ch = fgetc(fp)) != EOF))
+	while ((count < letters) && ((c = fgetc(fp)) != EOF))
 	{
-		putchar(ch);
+		putchar(c);
 		count++;
 	}
 	if (ferror(fp) != 0)
