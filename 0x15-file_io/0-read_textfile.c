cc5c7d39e251713433dd3ea1b842acc91fad6b39
craymahdee142 <craymahdaniel@gmail.com>
2023-05-08 21:49:54 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,16 +11,16 @@ size_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	size_t count = 0;
-	int c;
+	char ch;
 
 	if (filename == NULL)
 		return (0);
 	fp = fopen(filename, "r");
 	if (fp == NULL)
 		return (0);
-	while (count < letters && (c = fgetc(fp)) != EOF)
+	while ((count < letters) && (ch = fgetc(fp)) != EOF)
 	{
-		putchar(c);
+		putchar(ch);
 		count++;
 	}
 	if (ferror(fp) != 0)
@@ -29,5 +29,5 @@ size_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	fclose(fp);
-	return (count);	
+	return (count);
 }
