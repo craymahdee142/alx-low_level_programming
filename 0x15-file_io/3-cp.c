0424190269e2f7309c67528371eacbd6db03cd73
craymahdee142 <craymahdaniel@gmail.com>
2023-05-11 12:01:22 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,63 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *error_file - check if file can be opened.
- *@file_from: file_from.
- *@file_to: file
- *@argv: arguments vector.
- *Return: no return.
- */
-void error_file(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-/**
- *main - check the code for Holberton School students.
- *@argc: number of arguments.
- *@argv: arguments vector.
- *Return: Always 0.
+ *main - copy content of file_from to file_to
+ *@argc: num of arguments
+ *@argv: arguments
+ *Return: 0 on sucess, error or failure
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+	int fd_from, fd_to, bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	/* symbolic constants for permission bit for group, owner and others */
+	mode_t permissions = S_IRUSR | S_IWGRP | S_IROTH;
 
+	/* check if the number of arguments paased is correct */
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
+	/* open the source file for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
+		exit(98);
 	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
+	/* open a destination file for writing */
+	fd_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, permissions);
+	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
+		exit(99);
+	}
+	/* read data from source and write to destination */
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, WRITE_ERR, argv[2]);
+			exit(99);
+		}
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]);
+		exit(98);
+	}
+	/* close source file */
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, CLOSE_ERR, fd_from);
 		exit(100);
 	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
+	/* close destination file */
+	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
+	dprintf(STDERR_FILENO, CLOSE_ERR, fd_to);
+	exit(100);
 	}
 	return (0);
 }
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,6 +3,15 @@
 
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+#define READ_ERR "Error reading from file: %s\n"
+#define WRITE_ERR "Error writing to file: %s\n"
+#define CLOSE_ERR "Error closing file: %d\n"
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
