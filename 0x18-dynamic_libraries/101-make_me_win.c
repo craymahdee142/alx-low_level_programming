3a912a4083701862f45f295e8b26e77dfe3143a6
craymahdee142 <craymahdaniel@gmail.com>
2023-06-20 00:47:44 +0000

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/101-make_me_win.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ *malloc - allocate memmroy size
+ *Return: NULL
+ */
+void *malloc(size_t size)
+{
+	(void)size;
+	return (NULL);
+}
+/**
+ *rand - picks random number
+ *Return: int
+ */
+int rand(void)
+{
+	return (9);
+}
